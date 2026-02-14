f96c6fecfc6b46b9f117c5d4f960e13f8586938e
Elhadji N NGOM <ndongofa@hotmail.com>
2026-02-14 15:22:31 +0100

    Deploy Flutter web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "55c83b8a1d15e60accc1898e848e099d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d7ce3daaf46412bd0b182750cd414e43",
+"flutter_bootstrap.js": "c25ef50a02d1df7343046762ff010326",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
--- frontend/android/app/src/main/kotlin/com/example/nhtl_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nhtl_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
